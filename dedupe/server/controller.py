6b4e569dfc0b963350aa26101d032bded5b7f7e5
PoojaShah <shah.developer9@gmail.com>
2019-04-13 12:35:33 +0530

    server code commit

---
+++ dedupe/server/controller.py
@@ -0,0 +1,160 @@
+from future.builtins import next
+from flask import jsonify,json
+from itertools import combinations, groupby
+import os
+import csv
+import re
+import collections
+import logging
+import optparse
+from numpy import nan
+import datetime
+
+now = datetime.datetime.now()
+
+import dedupe
+from unidecode import unidecode
+
+input_file = 'inputfile/input.csv'
+
+settings_file = 'myTest_learned_settings'
+training_file = 'myTest_training.json'
+
+
+
+
+
+def preProcess(column):
+
+    import unidecode
+    #column = column.decode("utf8")
+    column = unidecode.unidecode(column)
+    column = re.sub('  +', ' ', column)
+    column = re.sub('\n', ' ', column)
+    column = column.strip().strip('"').strip("'").lower().strip()
+    if not column :
+        column = None
+    return column
+
+def readData(filename):
+
+    data_d = {}
+    with open(filename) as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            clean_row = [(k, preProcess(v)) for (k, v) in row.items()]
+            row_id = int(row['unique_id'])
+            data_d[row_id] = dict(clean_row)
+
+    return data_d
+
+def firstprogram():
+
+    import dedupe
+    # 1
+
+
+
+    pair = deduper.uncertainPairs()
+    return jsonify({'result' : pair[0]})
+
+def secondprogram(jsonfile):
+
+    #2
+    import dedupe
+    newinputs = jsonfile
+    newJson = {'distinct':[],'match':[]}
+    for dist in enumerate(newinputs['distinct']):
+        abc = list(dist)
+        xyz = tuple(abc[1])
+        newJson['distinct'].append(xyz)
+
+    for dist in enumerate(newinputs['match']):
+        abc = list(dist)
+        xyz = tuple(abc[1])
+        newJson['match'].append(xyz)
+
+    labeled_examples = newJson
+    deduper.markPairs(labeled_examples)
+
+    deduper.train()
+
+    with open(training_file, 'w') as tf :
+            deduper.writeTraining(tf)
+
+    threshold = deduper.threshold(data_d, recall_weight=2)
+
+    clustered_dupes = deduper.match(data_d, threshold)
+
+    cluster_membership = {}
+    cluster_notmember = {}
+    cluster_id = 0
+    for (cluster_id, cluster) in enumerate(clustered_dupes):
+        id_set, scores = cluster
+        """
+        id_tuple = list(id_set)
+        cluster_notmember[id_tuple[1]] = {}
+        id_tuple.pop()
+        id_set = tuple(id_tuple)
+        scores_tuple = list(scores)
+        scores_tuple.pop()
+        scores = tuple(scores_tuple)
+        """
+        cluster_d = [data_d[c] for c in id_set]
+        canonical_rep = dedupe.canonicalize(cluster_d)
+        for record_id, score in zip(id_set, scores) :
+            cluster_membership[record_id] = {
+                "cluster id" : cluster_id,
+                "canonical representation" : canonical_rep,
+                "confidence": score
+            }
+
+    singleton_id = cluster_id + 1
+
+    result = []
+    output_file = 'myTest_output' + str(now.day) + '.csv'
+    with open(output_file, 'w') as f_output:
+        writer = csv.writer(f_output)
+
+        with open(input_file) as f_input :
+            reader = csv.reader(f_input)
+
+            heading_row = next(reader)
+            heading_row.insert(0, 'confidence_score')
+            heading_row.insert(0, 'Cluster ID')
+            canonical_keys = canonical_rep.keys()
+            for key in canonical_keys:
+                heading_row.append('canonical_' + key)
+
+            writer.writerow(heading_row)
+
+            for row in reader:
+                row_id = int(row[0])
+                if row_id in cluster_membership :
+                    cluster_id = cluster_membership[row_id]["cluster id"]
+                    canonical_rep = cluster_membership[row_id]["canonical representation"]
+                    row.insert(0, cluster_membership[row_id]['confidence'])
+                    row.insert(0, cluster_id)
+                    for key in canonical_keys:
+                        row.append(canonical_rep[key].encode('utf8'))
+                elif row_id not in cluster_notmember :
+                    row.insert(0, None)
+                    row.insert(0, singleton_id)
+                    singleton_id += 1
+                    for key in canonical_keys:
+                        row.append(None)
+                writer.writerow(row)
+                result.append(row)
+
+    return (output_file)
+
+
+data_d = readData(input_file)
+
+variables = [{'field' : 'name', 'type': 'String'},
+             {'field' : 'address', 'type': 'String'},
+             {'field' : 'city', 'type': 'String', 'has missing':True},
+             {'field' : 'cuisine', 'type': 'String', 'has missing':True}]
+
+deduper = dedupe.Dedupe(variables)
+deduper.sample(data_d, 150000, .5)
---
+++ dedupe/server/index.py
@@ -0,0 +1,124 @@
+from flask import Flask
+from flask_cors import CORS
+from flask import jsonify
+from flask import request
+import boto3
+import uuid
+import csv
+import json
+
+from elasticsearch import Elasticsearch
+
+from controller import firstprogram,secondprogram
+
+from future.builtins import next
+
+import os
+import csv
+import re
+import collections
+import logging
+import optparse
+from numpy import nan
+
+import dedupe
+from unidecode import unidecode
+
+app = Flask(__name__)
+CORS(app)
+
+fields = []
+rows = []
+
+filepath = os.getcwd() + '/inputfile'
+
+if not os.path.exists(filepath):
+    os.makedirs(filepath)
+
+s3client = boto3.client('s3',aws_access_key_id='AKIAWQGV4HBLW42U7EUL',aws_secret_access_key='riihGrGkiIGHW0kqKSeBsvEW4M7cS3VpzAJMXSOa')
+s3client.download_file('pythoncsv','example.csv', filepath + '/input.csv')
+
+host = 'search-dedupe-n3y3uvp2uoiok2jgubwotjwag4.us-east-1.es.amazonaws.com'
+
+
+es = Elasticsearch(
+    hosts=[{'host': host, 'port': 443}],
+    use_ssl=True,
+    verify_certs=True
+)
+print(es.info())
+
+@app.route('/inputelastic', methods=['PUT'])
+def input():
+    print('input')
+    doc = {
+        'author': 'kimchy',
+        'text': 'Elasticsearch: cool. bonsai cool.',
+        'timestamp': "datetime.now()",
+    }
+
+    res = es.index(index="test-index", doc_type='tweet', body=doc)
+    #res = es.indices.create('test', body=doc)
+
+    return jsonify({'result' : res['result']})
+
+@app.route('/getelastic', methods=['GET'])
+def search():
+    res = es.search(index="csvresult", doc_type='records')
+
+    return jsonify({'result' : res['hits']['hits']})
+
+@app.route('/getcsv', methods=['GET'])
+def get_stars():
+  output = []
+  count = 0;
+  header = []
+  with open('inputfile/input.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    for row in readCSV:
+        if count == 0:
+            count = 1
+            for single in row:
+                header.append(single)
+        else:
+            final = {}
+            for index, item in enumerate(header):
+                 final[item]= row[index]
+            output.append(final)
+
+  return jsonify({'result' : output})
+
+@app.route('/getquestions', methods=['GET'])
+def get_questions():
+    questions = firstprogram()
+    return questions
+
+@app.route('/getnewcsv', methods=['POST'])
+def get_downloadcsv():
+    d = json.loads(request.data)
+    jsonfile = d['final']
+    xyz = secondprogram(jsonfile)
+    output = []
+    count = 0;
+    header = []
+    with open(xyz) as csvfile:
+      readCSV = csv.reader(csvfile, delimiter=',')
+      for row in readCSV:
+          if count == 0:
+              count = 1
+              for single in row:
+                  header.append(single)
+          else:
+              final = {}
+              for index, item in enumerate(header):
+                   final[item]= row[index]
+              output.append(final)
+
+    doc = {'result' : output}
+    print(doc)
+    res = es.index(index="csvresult", doc_type='records', body=doc)
+    return jsonify({'result' : output})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
